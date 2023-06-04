f8cc1ac15c07b85984f264dd0319bea012e1d776
bmvaldivieso <bmvaldivieso@utpl.edu.ec>
2023-06-03 19:48:45 -0500

    appflutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:appflutter/screens/carnet_page.dart';
+
+import 'package:appflutter/screens/counter_page.dart';
+import 'package:appflutter/screens/listado_noticias.dart';
+import 'package:appflutter/screens/menu_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const MenuPage(),
+      routes: {
+        '/contador': (BuildContext context) => const CounterPage(),
+        '/carnet': (BuildContext context) => const CarnetPage(),
+        '/noticias': (BuildContext context) => ListadoNoticias(),
+      },
+    );
+  }
+}
